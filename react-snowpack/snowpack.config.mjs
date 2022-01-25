596857f57bd83d1a6a0d14a3c095570096a537f3
Jay Harris <jaywritescode@users.noreply.github.com>
2022-01-24 20:48:26 -0500

    bootstrap a react app

--- react-snowpack/snowpack.config.mjs
+++ react-snowpack/snowpack.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    public: '/',
-    src: '/dist',
+    public: "/",
+    src: "/dist",
   },
-  plugins: ['@snowpack/plugin-react-refresh'],
+  plugins: ["@snowpack/plugin-react-refresh"],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
