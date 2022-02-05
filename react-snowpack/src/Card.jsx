ade1395757c51d2eacba236880f5e0dd21fc51cc
Jay Harris <jaywritescode@users.noreply.github.com>
2022-02-05 00:08:37 -0500

    click to select

--- react-snowpack/src/Card.jsx
+++ react-snowpack/src/Card.jsx
@@ -4,12 +4,13 @@ import classNames from "classnames";
 import "./Card.css";
 
 function Card(props) {
-  const { card, isSelected } = props;
+  const { card, isSelected, onClick } = props;
 
   return (
     <div
       className={classNames("card", "column", { isSelected })}
       style={{ backgroundImage: `url(cards/${card}.png)` }}
+      onClick={onClick}
     />
   );
 }
