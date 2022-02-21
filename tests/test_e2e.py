4fdf91b5759ab52888c76c137a33eaa345e33b26
Jay Harris <jaywritescode@users.noreply.github.com>
2022-02-21 11:11:13 -0500

    start uvicorn in a separate process, then test

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,26 +1,21 @@
-# import asyncio
-# from playwright.async_api import async_playwright
+from multiprocessing import Process
 import pytest
 from starlette.testclient import TestClient
 import uvicorn
 
 from web.api import app
 
-@pytest.fixture
-def test_app():
-    with uvicorn.run(app, host='0.0.0.0', port=3001) as client:
-        yield client
 
-def test_example_is_working(test_app, page):
+def run_server():
+    uvicorn.run(app, host='0.0.0.0', port=3001)
+
+@pytest.fixture
+def server():
+    proc = Process(target=run_server, args=(), daemon=True)
+    proc.start()
+    yield
+    proc.kill()
+
+def test_example_is_working(server, page):
     page.goto("http://localhost:3001")
     assert page.title() == "set game"
-
-# async def main():
-#     async with async_playwright() as p:
-#         browser = await p.chromium.launch()
-#         page = await browser.new_page()
-#         await page.goto("http://playwright.dev")
-#         print(await page.title())
-#         await browser.close()
-
-# asyncio.run(main())
