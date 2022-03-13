47e21124a7ff6e98a9e0703d604a0cd2da0ab196
Jay Harris <jaywritescode@users.noreply.github.com>
2022-03-13 16:48:07 -0400

    clean up players section (mobile)

--- react-snowpack/src/Players.jsx
+++ react-snowpack/src/Players.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import classNames from "classnames";
 import { toPairs } from "ramda";
 
+import "purecss";
+import "./players.css";
+
 export default function Players(props) {
   const { players, myName } = props;
 
   return (
-    <div className="players">
-      <h4>Players</h4>
+    <div className="players pure-g">
+      <div className="header pure-u-1">Players</div>
       {toPairs(players).map(([name, setsFound]) => (
         <li
-          className={classNames("player", {
+          className={classNames("player", "pure-u-1-4", {
             myself: name == myName,
           })}
           key={name}
         >
-          <div>{name}</div>
-          <div>{setsFound.length}</div>
+          <span>{name}</span>
+          <span>{setsFound.length}</span>
         </li>
       ))}
     </div>
